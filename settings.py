a868aa2a8082f2a68565b9d72b7e817aec72b9d1
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-10-28 20:35:38 +0100

    Adapt TEMPLATE_DIRS.

--- settings.py
+++ settings.py
@@ -69,7 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/julen/dev/hitzokei/templates/",
+    "/home/www/hitzokei/public_html/hitzokei/templates/",
 )
 
 INSTALLED_APPS = (
