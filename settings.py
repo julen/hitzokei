738c6b3c7de84e18f635212d0c7f4090df3c2746
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-10-28 20:43:55 +0100

    Adjust crontab path.

--- settings.py
+++ settings.py
@@ -35,12 +35,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/www/hitzokei/hitzokei/static/'
+MEDIA_ROOT = '/home/julen/dev/hitzokei/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://hitzokei.tximinia.net/static/'
+MEDIA_URL = 'http://localhost:8000/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -69,7 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/www/hitzokei/hitzokei/templates/",
+    "/home/julen/dev/hitzokei/templates/",
 )
 
 INSTALLED_APPS = (
